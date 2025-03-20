ad2305990a90a3db02d029788c055e208d1cae9e
Jaime Nizama <jamesnizama20@gmail.com>
2025-03-20 10:22:21 -0500

    49-@PreAuthorize

--- src/main/java/com/mitocode/controller/CategoryController.java
+++ src/main/java/com/mitocode/controller/CategoryController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,6 +30,8 @@ public class CategoryController {
     private final ICategoryService service;
     private final MapperUtil mapperUtil;
 
+    @PreAuthorize("@authService.hasAccess()")
+    //@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
     @GetMapping
     public ResponseEntity<List<CategoryDTO>> readAll() throws Exception {
 //        List<CaregoryRecord> list = service.findAll()
